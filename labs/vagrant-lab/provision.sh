e0ca472ad3e0244a4a3ec76ea41043033f5b27cd
M.BETTAIEB <bettaiebmedaly@gmail.com>
2026-09-06 18:36:51 +0000

    update

--- labs/vagrant-lab/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y nginx
-
-echo "<h1>Serveur Web provisionné avec Vagrant</h1>" > /var/www/html/index.html
--- labs/vagrant-lab/scripts/web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y nginx
-
-echo "<h1>Web Server</h1>" > /var/www/html/index.html
